419173134230570060ede9fffdbf335c5b46a174
Steven Decock <steven.decock@vdab.be>
2018-10-11 15:54:31 +0200

    [Steven] Test webcomponent. Not working in Chrome now. Only Edge.

--- angular-element-test/src/app/app.component.ts
+++ angular-element-test/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Input, Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'custom-button',
+  template: `<button (click)="handleClick()">{{label}}</button>`,
+  styles: [`
+    button {
+      border: solid 3px;
+      padding: 8px 10px;
+      background: #bada55;
+      font-size: 20px;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'My First Angular App!';
+  @Input() label = 'default label';
+  @Output() action = new EventEmitter<number>();
+  private clicksCt = 0;
+
+  handleClick() {
+    this.clicksCt++;
+    this.action.emit(this.clicksCt);
+  }
 }
--- angular-element-test/src/app/app.module.ts
+++ angular-element-test/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector  } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,16 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) {
+    const customButton = createCustomElement(AppComponent, { injector });
+    customElements.define('custom-button', customButton);
+  }
+
+  // tell Angular to use this module for bootstrapping
+  ngDoBootstrap() {}
+
+}
